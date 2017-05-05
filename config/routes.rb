725eb336a3f48f206c99cc1531dc4ab1d0e23d90
hkarambizi <hkarambizi@gmail.com>
2017-05-05 17:31:32 -0400

    added more gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'client#index'
-get '*path', to: 'client#index'
+  root to: 'client#dashboard'
+get '*path', to: 'client#dashboard'
 end
