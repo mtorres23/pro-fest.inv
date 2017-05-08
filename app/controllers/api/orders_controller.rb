eafb45959a9c36f01781cab8a1b920eccb1f6c08
ddmilo <ddmilo1029@gmail.com>
2017-05-08 14:57:32 -0400

    added tmp links to api_order views

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -5,11 +5,29 @@ class Api::OrdersController < ApplicationController
   end
 
   def new
+    @orders = Order.new
+
+    render :new
   end
 
   def create
+    puts "create"
+    puts @orders
+    order = Order.create(order_params)
+    order.save
+
+    redirect_to api_orders_path
   end
 
   def show
   end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:content, :verified_by, :delivery_date, :total_price, :total_amount, :type)
+
+    end
+
 end
