d7387df8e79ba1fb83c3c26b7b20f4d41eca4bc3
hkarambizi <hkarambizi@gmail.com>
2018-09-14 13:21:33 -0400

    debugged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery prepend: true
   before_action :set_client
  
 
-def set_client
-  client =  Client.first
-  @client= Client.find(client.id)
+  def set_client
+    client =  Client.first
+    @client= Client.find(client.id)
+  end
+
+protected
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:client_id])
+  end
 end
 
-end
