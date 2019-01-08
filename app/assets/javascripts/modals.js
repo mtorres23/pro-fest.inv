386df46f2524fd37b6ae1a22f2bf4344cae1331b
hkarambizi <hkarambizi@gmail.com>
2019-01-07 21:18:52 -0500

    merge

--- app/assets/javascripts/modals.js
+++ app/assets/javascripts/modals.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
 // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
 $('select').material_select();
-// $('.modal').openModal();
+
 var $modal1 = $('#modal1');
 var $modal2 = $('#modal2');
-$('#modal1').openModal();
+$('#modal1').openModal({
+  dismissable: false
+});
 
 var toggleMap = function() {
  
  if ($modal1.open) {
     $modal1.closeModal();
-    $modal2.openModal();
+    // $modal2.openModal();
   } else {
     $modal1.openModal();
   }
@@ -33,7 +35,7 @@ $('.map-add').on('click', function(){
 
       // Open the infowindow and populate the form with the form data
 
-      
+
 });
 
 // Edit on map
