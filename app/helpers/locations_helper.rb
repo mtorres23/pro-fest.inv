547002fffd46d7f4473b2a05137f5e8f039953b5
hkarambizi <hkarambizi@gmail.com>
2020-03-03 02:35:43 -0500

    Location and Event Menu Styling Updates
    - menu buttons for location show
    - menu buttons for events
    - new event index page

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -15,9 +15,20 @@ module LocationsHelper
         when 'storage'
             return 'https://cdn2.iconfinder.com/data/icons/seo-and-marketing-line-5/32/100-512.png'
         when 'compound'
-            return 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJ81v7KinpZwPfLAmPDeUdRprJFbk2MCwn3b4gf79w6C8QMZai'
+            return 'https://i.ya-webdesign.com/images/baseball-plate-png-10.png'
         when 'truck'
             return 'https://icons-for-free.com/iconfiles/png/512/shipping+store+truck+icon-1320166084459861055.png'
+        end
+    end
+
+    def location_menu_button(option)
+        case option
+        when 'Inventory'
+            return "https://cdn3.iconfinder.com/data/icons/food-technology-2/64/Shelves_Food-shelving-supermarket-drink-512.png"
+        when "Staff"
+            return 'https://cdn.iconscout.com/icon/premium/png-256-thumb/staff-9-559919.png'
+        when "Orders"
+            return 'https://lh3.googleusercontent.com/proxy/bPBsVsLdZGZt2ZZl-OW3IKopdrf9qka9rl_LyDPn7FxXNL1MyaJRH-VHTof9mKk2Hyma2uooDNuIzjwI8O1N9BsyxXQoyWemKwHw6a11bvogmtzLrl0yhgUcNDUZ0ppnxSoCrfPVpW8q'
         end
     end
 end
